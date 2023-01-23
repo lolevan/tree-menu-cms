f2bfa54013e5456fcadaa0c6bb1e3e7b960e187b
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-01-23 20:31:01 +0800

    Refactor: add methods and add comments

--- project/menu_cms/models.py
+++ project/menu_cms/models.py
@@ -2,14 +2,61 @@ from django.db import models
 
 
 class Menu(models.Model):
-    name = models.CharField(verbose_name='Menu name')
-    display_name = models.CharField()
-    root_node = models.ForeignKey(to='Node', on_delete=models.CASCADE)
+    """
+    class describing the properties menu
+    """
+    name = models.CharField(
+        verbose_name='Menu name',
+        max_length=100,
+        unique=True,
+    )
+    display_name = models.CharField(
+        verbose_name='Menu display',
+        max_length=100,
+    )
+    root_node = models.ForeignKey(
+        to='Node',
+        on_delete=models.CASCADE,
+        related_name='is_rood_node',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Node(models.Model):
-    node_name = models.CharField()
-    named_url = models.URLField()
-    url = models.URLField()
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
-    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+    """
+    class describing the properties node
+    """
+    node_name = models.CharField(
+        verbose_name='Node_name',
+        max_length=255,
+    )
+    named_url = models.CharField(
+        verbose_name='named url default',
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    url = models.CharField(
+        verbose_name='URL',
+        blank=True,
+        null=True,
+    )
+    parent = models.ForeignKey(
+        to='self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    menu = models.ForeignKey(
+        to='Menu',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.node_name
