538c68a0bb429bb557bee8c2efdc79728ad90d40
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-01-23 19:24:18 +0800

    Create menu and node models

--- project/menu_cms/models.py
+++ project/menu_cms/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(verbose_name='Menu name')
+    display_name = models.CharField()
+    root_node = models.ForeignKey(to='Node', on_delete=models.CASCADE)
+
+
+class Node(models.Model):
+    node_name = models.CharField()
+    named_url = models.URLField()
+    url = models.URLField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
