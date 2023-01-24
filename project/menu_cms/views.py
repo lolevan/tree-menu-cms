a3ae67f2907afd71f44894eb40bce25e24dd4d4f
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-01-24 19:51:46 +0800

    feat: Create base function and url

--- project/menu_cms/views.py
+++ project/menu_cms/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def test_menu_view(request):
+    context = {'menu': 'Test menu'}
+    return render(request, 'menu.html', context)
