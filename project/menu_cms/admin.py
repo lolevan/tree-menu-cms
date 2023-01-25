8359e15e1fcfb73eed30805b42e19cce909f714b
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-01-25 21:49:09 +0800

    feat: Create base admin.py

--- project/menu_cms/admin.py
+++ project/menu_cms/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, Node
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'display_name', 'root_node']
+    list_display_links = ['pk', 'name']
+    readonly_fields = ['root_node']
+
+
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'node_name', 'named_url', 'url', 'menu', 'parent']
+    list_display_links = ['pk', 'node_name']
+    readonly_fields = ['menu', 'parent']
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Node, NodeAdmin)
