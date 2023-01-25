ce22ee069be0c995fbc416f0082c71680f7fefe8
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-01-25 22:01:44 +0800

    refactor: Edit admin.py
    Add list_editable in NodeAdmin and add inlines class

--- project/menu_cms/admin.py
+++ project/menu_cms/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 from .models import Menu, Node
 
 
+class NodesInline(admin.StackedInline):
+    model = Node
+    extra = 2
+    readonly_fields = [
+        'menu',
+        'parent',
+    ]
+
+
 class MenuAdmin(admin.ModelAdmin):
     list_display = ['pk', 'name', 'display_name', 'root_node']
     list_display_links = ['pk', 'name']
@@ -12,7 +21,9 @@ class MenuAdmin(admin.ModelAdmin):
 class NodeAdmin(admin.ModelAdmin):
     list_display = ['pk', 'node_name', 'named_url', 'url', 'menu', 'parent']
     list_display_links = ['pk', 'node_name']
+    list_editable = ['named_url', 'url']
     readonly_fields = ['menu', 'parent']
+    inlines = [NodesInline]
 
 
 admin.site.register(Menu, MenuAdmin)
