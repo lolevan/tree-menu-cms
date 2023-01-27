e7b0476cc8b6e49886bdbc43380cad73f8e9e283
Ivan Nikitin <vanechka-nikitin-2004@mail.ru>
2023-01-28 00:10:19 +0800

    refactor: Edit views

--- project/menu_cms/views.py
+++ project/menu_cms/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def test_menu_view(request):
     context = {'menu': 'Test menu'}
-    return render(request, 'menu.html', context)
+    return render(request, 'menu_cms/menu.html', context)
